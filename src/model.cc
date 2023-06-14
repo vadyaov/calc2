d14ee6ebba55e67ca1cabfa43bf53bb6e5d2e2e1
vadim <vamyakish@mail.ru>
2023-06-15 00:55:50 +0300

    polish

--- src/model.cc
+++ src/model.cc
@@ -1,84 +1,126 @@
-#include "model.h"
 #include <iostream>
 #include <vector>
 #include <stack>
-
 #include <cctype>
 
-static bool IsDigit(const char ch) {
+#include "model.h"
+
+static bool IsDigit(char ch) {
   return std::isdigit(static_cast<unsigned char>(ch));
 }
 
-static bool IsAlpha(const char ch) {
+static bool IsAlpha(char ch) {
   return std::isalpha(static_cast<unsigned char>(ch));
 }
 
-static bool IsFunction(const char ch) {
-  return std::string("sctSCTqlg").find_first_of(ch);
+static bool IsFunction(char ch) {
+  return std::string("sctSCTqlg").find(ch) == std::string::npos ? false : true;
 }
 
-static bool IsOperator(const char ch) {
-  std::string s {"|~m+-/*^"};
-  return s.find(ch) == std::string::npos ? false : true;
+static bool IsOperator(char ch) {
+  return std::string("|~m+-/*^").find(ch) == std::string::npos ? false : true;
 }
 
-int SetPriority(const char c) {
+static int SetPriority(char c) {
   int p = 0;
-  if (c == '*' || c == '/')
-    p = 1;
-  else if (c == '^' || c == 'm')
-    p = 2;
-  else if (c == '~' || c == '|')
-    p = 3;
+
+  if (c == '*' || c == '/') p = 1;
+  else if (c == '^' || c == 'm') p = 2;
+  else if (c == '~' || c == '|') p = 3;
+
   return p;
 }
 
-static std::size_t FillDigit(std::string::const_iterator it, std::string& out) {
-  std::size_t sz = 0;
-  while (IsDigit(*it)) {
-    out.push_back(*it);
-    ++sz;
-    ++it;
-  }
+void AppendToOutput(char c, std::string& out) {
+  out.push_back(c);
+}
+
+void AppendSpaceToOutput(std::string& out) {
+  out.push_back(' ');
+}
+
+static void AppendNumber(std::string::const_iterator& it, std::string& out) {
+  while (IsDigit(*it)) out.push_back(*it++);
+
   if (*it == '.') {
-    out.push_back(*it++);
-    ++sz;
+    AppendToOutput(*it++, out);
     if (!IsDigit(*it)) throw std::logic_error("Invalid expression");
-    while (IsDigit(*it)) {
-      out.push_back(*it);
-      ++sz;
-      ++it;
+
+    while (IsDigit(*it)) AppendToOutput(*it++, out);
+  }
+  --it;
+  AppendSpaceToOutput(out);
+}
+
+void ProcessClosingBracket(std::string& out, std::stack<char>& sstack) {
+  while (sstack.top() != '(') {
+    AppendToOutput(sstack.top(), out);
+    AppendSpaceToOutput(out);
+    sstack.pop();
+    if (sstack.empty()) throw std::logic_error("Incorrect input");
+  }
+  sstack.pop();
+}
+
+void ProcessOperator(char c, std::string& out, std::stack<char>& sstack) {
+  int priority = SetPriority(c);
+  if (!sstack.empty()) {
+    char b = sstack.top();
+    while (!sstack.empty() && (IsFunction(b) ||
+          (IsOperator(b) && SetPriority(b) >= priority && priority != 3))) {
+      if (priority == 2 && c == b) break;
+      AppendToOutput(b, out);
+      AppendSpaceToOutput(out);
+      sstack.pop();
+      if (!sstack.empty()) b = sstack.top();
     }
   }
-  return sz;
+  sstack.push(c);
+}
+
+void ProcessRemainingOperators(std::string& out, std::stack<char>& sstack) {
+  while (!sstack.empty()) {
+    char b = sstack.top();
+    if (IsOperator(b) || IsFunction(b)) {
+      AppendToOutput(b, out);
+      sstack.pop();
+      if (!sstack.empty()) AppendSpaceToOutput(out);
+    } else {
+      throw std::logic_error("Incorrect expression");
+    }
+  }
 }
 
 static std::string MakePolish(const std::string& src) {
   std::string out;
-
   std::stack<char> sstack;
 
   for (std::string::const_iterator i = src.cbegin(); i != src.cend(); ++i) {
     if (*i == 'x') {
-      out.push_back(*i);
+      AppendToOutput(*i, out);
     } else if (IsDigit(*i)) {
-      i += FillDigit(i, out);
+      AppendNumber(i, out);
     } else if (IsFunction(*i) || *i == '(') {
       sstack.push(*i);
     } else if (*i == ')') {
-      while (sstack.top() != '(') {
-        out.push_back(sstack.top());
-        sstack.pop();
-      }
-      sstack.pop();
+      ProcessClosingBracket(out, sstack);
     } else if (IsOperator(*i)) {
-      int priority = SetPriority(*i);
-      // continue here
+      ProcessOperator(*i, out, sstack);
     }
   }
 
-  std::cout << "out:" << out << std::endl;
+  ProcessRemainingOperators(out, sstack);
+
   return out;
+}
+
+static void FindUnary(std::string& src) {
+  for (std::size_t i = 0; i < src.size(); ++i)
+    if (src[i] == '+' || src[i] == '-')
+      if (i == 0 || src[i - 1] == '(' || IsOperator(src[i - 1]) || src[i - 1] == 'm') {
+        if (src[i] == '-') src[i] = '~';
+        else src[i] = '|';
+      }
 }
 
 static std::string ReplaceFunctionsWithSymbols(const std::string& src) {
@@ -96,6 +138,9 @@ static std::string ReplaceFunctionsWithSymbols(const std::string& src) {
         }
     result += src[i];
   }
+
+  FindUnary(result);
+
   return result;
 }
 
@@ -108,5 +153,6 @@ Model::Model(const std::string& src) : expression{src} {
 double Model::CalculateExpression() const {
   std::cout << "Expression:" << expression << std::endl;
   std::string polish_string = MakePolish(expression);
+  std::cout << "Polistring:" << polish_string << std::endl;
   return 0.0;
 }
